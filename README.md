ecb00131b81639d53506afa89e97551f951e628a
yujun <yujun505646964@gmail.com>
2021-02-23 16:46:50 +0800

    使用@sentinelResource注册 定义资源。测试限流的处理

--- README.md
+++ README.md
@@ -8,15 +8,22 @@ spring.cloud.sentinel.filter.enabled注解原理，开启关闭对比
 根据项目需要，如果全部接口都要，则开启true。部分接口限流，用
 @SentinelResource注解 
 
+spring.cloud.sentinel.filter.enabled =false
+为什么控制台流控规则不能点？
 
 
 # 延伸阅读
 
 https://mp.weixin.qq.com/s/AjHCUmygTr78yo9yMxMEyg
 
+sentinel-dashboard持久化到nacos
 https://blog.csdn.net/nesrpk/article/details/107294409
 
+接口流控处理优化
 https://blog.csdn.net/luanlouis/article/details/91633042
+@SentinelResource注解
+https://blog.csdn.net/lzb348110175/article/details/107634024
+https://www.jianshu.com/p/b61727df6ea5
 
 
 
--- spring-cloud/src/main/java/com/example/demo/controller/ConsumerController.java
+++ spring-cloud/src/main/java/com/example/demo/controller/ConsumerController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/spring")
+@Slf4j
 public class ConsumerController {
 
-    @SentinelResource(value = "/spring/hello")
+    /*
+     * blockHandler 方法必须public ,切返回值相同，参数相同，异常
+     * */
+    @SentinelResource(value = "/spring/hello", blockHandler = "exceptionHandler")
     @GetMapping("/hello")
     public String hello() {
         return "你好,hello";
@@ -30,4 +37,13 @@ public class ConsumerController {
         return "DELETE 请求";
     }
 
+
+    public String exceptionHandler(BlockException ex) {
+        System.out.println("blockHandler");
+        return "blockHandler";
+    }
+
+    public void fallbackHandler(String str) {
+        log.error("fallbackHandler：" + str);
+    }
 }
