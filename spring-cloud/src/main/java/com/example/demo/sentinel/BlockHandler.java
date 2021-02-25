cc7236710176e9c2b22c39f8e82bf006a466b406
yujun <yujun505646964@gmail.com>
2021-02-25 09:49:08 +0800

    验证系统规则，新增一些參數説明

--- spring-cloud/src/main/java/com/example/demo/sentinel/BlockHandler.java
+++ spring-cloud/src/main/java/com/example/demo/sentinel/BlockHandler.java
@@ -3,7 +3,8 @@ package com.example.demo.sentinel;
  * blockHandler
     1.方法必须public,不在同一个类中，还必须为static
     2.返回值相同，参数列表相同，可以在参数最后多带一个参数，类型为异常BlockException
-    3.
+    * (com.alibaba.csp.sentinel.slots.system.SystemBlockException系统规则也会被捕获)
+    3.SystemBlockException：只对sentinel resource资源起作用，但是当非资源打满系统负载，就可能导致重要资源接口被系统规则限流
  */
 
 import com.alibaba.csp.sentinel.slots.block.BlockException;
